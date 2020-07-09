6aea69731d751c556dc71d33ce25130794bdb24d
Pivithuru Jayakody <piveedj@gmail.com>
2020-07-09 12:58:08 +0530

    Create IUser and User entity

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn
+} from "typeorm";
+
+@Entity()
+class User {
+
+  @PrimaryGeneratedColumn()
+  public id: string;
+
+  @Column()
+  public name: string;
+
+  @Column()
+  public email: string;
+
+  @Column()
+  public password: string;
+
+}
+
+export default User;
