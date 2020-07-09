b93b27aa70d64b5117a55a1d0d7cdde6a3c4382e
Pivithuru Jayakody <piveedj@gmail.com>
2020-07-09 11:34:24 +0530

    Create Post entity

--- src/routes/post/post.controller.ts
+++ src/routes/post/post.controller.ts
@@ -1,6 +1,12 @@
 import * as express from "express";
+
+import {
+  getRepository
+} from "typeorm";
+
 import Controller from "../../interfaces/controller.interface";
-import Post from "../../interfaces/post.interface";
+import IPost from "../../interfaces/post.interface";
+import Post from "./post.entity";
 
 //------------------------------------------------
 // #region ----------------------------------- //. 🔻 ⚙ Middlewares
@@ -24,8 +30,9 @@ class PostController implements Controller {
 
   public path = `/post`;
   public router = express.Router();
+  private postRepository = getRepository(Post);
 
-  private postList: Post[] = [
+  private postList: IPost[] = [
     {
       author: `Pivithuru`,
       content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, eius corrupti. Libero quisquam vero temporibus eveniet ut consequatur, impedit optio delectus enim inventore vel, laborum aperiam neque quos non ad!`,
@@ -41,29 +48,63 @@ class PostController implements Controller {
 
   public intializeRoutes(): void {
 
+    /*
+     * Create
+     */
+    this.router.post(
+      `${this.path}`,
+      validationMiddleware(CreatePostDTO),
+      this.createPost
+    );
+    /*
+     * Get All
+     */
     this.router.get(
       `${this.path}/list`,
       this.getAllPosts
     );
-    this.router.post(
-      `${this.path}/create`,
-      validationMiddleware(CreatePostDTO),
-      this.createAPost
+    /*
+     * Get One by ID
+     */
+    this.router.get(
+      `${this.path}/:id`,
+      this.getPostById
+    );
+    /**
+     * Update One by ID
+     */
+    this.router.patch(
+      `${this.path}/:id`,
+      validationMiddleware(CreatePostDTO, true),
+      this.modifyPost
+    );
+    /**
+     * Delete One by ID
+     */
+    this.router.delete(
+      `${this.path}/:id`,
+      this.deletePost
     );
 
   }
 
-  getAllPosts = (request: express.Request, response: express.Response) => {
+  createPost = (
+    request: express.Request,
+    response: express.Response
+  ): void => {
 
-    response.send(this.postList);
+    const post: IPost = request.body;
+    this.postList.push(post);
+    response.send(post);
 
   }
 
-  createAPost = (request: express.Request, response: express.Response) => {
+  getAllPosts = (
+    request: express.Request,
+    response: express.Response
+  ): void => {
 
-    const post: Post = request.body;
-    this.postList.push(post);
-    response.send(post);
+    response.send(this.postList);
 
   }
 
